d274bd2b71e2457df9ffaecb174b6cabbc1cd837
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-13 17:09:36 +0530

    UC1 has been created where the values are pushed onto the stack

--- src/com/stackqueue/Main.java
+++ src/com/stackqueue/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to stacks and queue using linked list");
+		
+		Stack stack = new Stack();
+		
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		
+		stack.print();
 	}
+	
 
 }
